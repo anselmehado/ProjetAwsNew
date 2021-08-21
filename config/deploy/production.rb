8a81440a9ac2d585a26212fb9fd80c744fc87649
hado anselme <anselmehado85@yahoo.com>
2021-08-21 11:32:18 +0100

    init unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,6 @@
-server'3.212.197.144', user:'anselme', roles: %w{anselme db web}
- set :ssh_options, keys:'"/home/hado/.ssh" anselme@3.212.197.144'
+server '3.212.197.144', user:'anselme', roles: %w{anselme db web}
+#set :ssh_options, keys: '/Users/Akihiro/.ssh/id_rsa'
+set :ssh_options, keys:'/home/hado/.ssh/id_rsa'
+
+ # server'3.212.197.144', user:'anselme', roles: %w{anselme db web}
+ #  set :ssh_options, keys:'"/home/hado/.ssh" anselme@3.212.197.144'
