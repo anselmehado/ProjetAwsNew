a89dbda8423f56752e78eeb5a4f40d58b2459488
hado anselme <anselmehado85@yahoo.com>
2021-08-21 15:30:06 +0100

    gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 # config valid only for current version of Capistrano
-lock '3.6.0'
+#lock '3.6.0'
+lock '3.16.0'
+#3.16.0
 # Application name to deploy
 set :application, 'ProjetAwsNew'
 # git repository to clone
